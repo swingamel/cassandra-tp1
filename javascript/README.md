4411104ed3cd8b9bcc90bee6fdde1d329ccceafc
Sounalet Alexandre <alex.sounalet@hotmail.fr>
2023-01-17 15:45:02 +0100

    Javascript Select

---
+++ javascript/README.md
@@ -0,0 +1,16 @@
+# Version Javascript
+## Liste les données d'une table choisie
+- Commandes à lancer
+```
+npm i
+npm run start
+```
+
+- .env à configurer
+```
+// Nom du keyspace
+KEYSPACE=""
+
+// Nom de la table
+TABLE_NAME=""
+```
--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,15 @@
+const env = require('dotenv');
+env.config();
+
+// Connexion à la base cassandra
 let cassandra = require('cassandra-driver');
-
-// Replace 'Username' and 'Password' with the username and password from your cluster settings
 let authProvider = new cassandra.auth.PlainTextAuthProvider('root', 'root');
-// Replace the PublicIPs with the IP addresses of your clusters
 let contactPoints = ['127.0.0.1:9042', '127.0.0.1:9043', '127.0.0.1:9044'];
-// Replace DataCenter with the name of your data center, for example: 'AWS_VPC_US_EAST_1'
 let localDataCenter = 'datacenter1';
+let client = new cassandra.Client({ contactPoints: contactPoints, authProvider: authProvider, localDataCenter: localDataCenter, keyspace: process.env.KEYSPACE });
 
-let client = new cassandra.Client({ contactPoints: contactPoints, authProvider: authProvider, localDataCenter: localDataCenter, keyspace: 'grombluche' });
-// Define and execute the queries
-
-let query = 'SELECT name, type FROM grombluche.miamonx ALLOW FILTERING';
-let q1 = client.execute(query, []).then(result => { console.table(result.rows); }).catch((err) => { console.log('Erreur à la récupération de la colonne', err); });
+// Requête de selection
+const query = `SELECT name, type FROM ${process.env.KEYSPACE}.${process.env.TABLE_NAME}`;
+let q1 = client.execute(query, []).then(result => { console.table(result.rows); }).catch((err) => { console.log('Erreur à la récupération de la colonne : ', err); });
 // Exit the program after all queries are complete
 Promise.allSettled([q1]).finally(() => client.shutdown());
