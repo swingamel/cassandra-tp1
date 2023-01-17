70e77c0eea1b2cb026e916e36bb6825f8d4df3e0
Sounalet Alexandre <alex.sounalet@hotmail.fr>
2023-01-17 16:15:55 +0100

    Base de code plus propre

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,19 @@
+// .env
 const env = require('dotenv');
 env.config();
 
-// Connexion à la base cassandra
-let cassandra = require('cassandra-driver');
-let authProvider = new cassandra.auth.PlainTextAuthProvider(process.env.DB_USER, process.env.DB_PASS);
-let contactPoints = ['127.0.0.1:9042', '127.0.0.1:9043', '127.0.0.1:9044'];
-let localDataCenter = 'datacenter1';
-let client = new cassandra.Client({ contactPoints: contactPoints, authProvider: authProvider, localDataCenter: localDataCenter, keyspace: process.env.KEYSPACE });
+const cassandra = require('cassandra-driver');
 
-// Requête de selection
+// Connexion à la base
+const client = new cassandra.Client({
+    contactPoints: ['127.0.0.1:9042', '127.0.0.1:9043', '127.0.0.1:9044'],
+    localDataCenter: 'datacenter1',
+    keyspace: process.env.KEYSPACE,
+    credentials: { username: process.env.DB_USER, password: process.env.DB_PASS }
+});
+
+// Selection de la table
 const query = `SELECT * FROM ${process.env.KEYSPACE}.${process.env.TABLE_NAME}`;
-let q1 = client.execute(query, []).then(result => { console.table(result.rows); }).catch((err) => { console.log('Erreur à la récupération de la colonne : ', err); });
-// Exit the program after all queries are complete
-Promise.allSettled([q1]).finally(() => client.shutdown());
+
+// Execution de la requête
+client.execute(query, []).then(result => console.table(result.rows));
