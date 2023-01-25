0a3bfedce08d15dcc6cf215c77462bdbab67b234
Sounalet Alexandre <alex.sounalet@hotmail.fr>
2023-01-25 14:42:38 +0100

    JS

--- javascript/index.js
+++ javascript/index.js
@@ -2,18 +2,102 @@
 const env = require('dotenv');
 env.config();
 
+const crypto = require('crypto');
+
 const cassandra = require('cassandra-driver');
+
+const name = 'vm';
 
 // Connexion à la base
 const client = new cassandra.Client({
     contactPoints: ['127.0.0.1:9042'],
     localDataCenter: 'datacenter1',
-    keyspace: process.env.KEYSPACE,
+    keyspace: 'system',
     credentials: { username: process.env.DB_USER, password: process.env.DB_PASS }
 });
 
-// Selection de la table
-const query = `SELECT * FROM ${process.env.KEYSPACE}.${process.env.TABLE_NAME}`;
+/**
+ * listKeyspaces() retourne l'objet de tous les keyspaces de la base de données
+ */
+async function listKeyspaces() {
+    const keySpaces = await client.execute('DESCRIBE KEYSPACES');
+    console.table(keySpaces.rows);
+}
+listKeyspaces();
 
-// Execution de la requête
-client.execute(query, []).then(result => console.table(result.rows));
+
+/**
+ * main() gère la différentes requêtes à effectuer sur la base
+ */
+function main() {
+    // Ajoute le keyspace
+    createKeyspace(`${name}_user`);
+
+    // Ajoute la table 
+    createTable(`${name}_user`, `${name}_cfuser`);
+
+    // Vide la table
+    truncateTable(`${name}_user`, `${name}_cfuser`);
+
+    // Ajout d'un utilisateur
+    createUser(`${name}_user`, `${name}_cfuser`, crypto.randomUUID(), 'Sounalet', 'Alexandre', 'miamo.fr', '2002-02-01', 0);
+    createUser(`${name}_user`, `${name}_cfuser`, crypto.randomUUID(), 'Duchesne', 'Tom', 'tom.feur', '1947-02-17', 0);
+    createUser(`${name}_user`, `${name}_cfuser`, crypto.randomUUID(), 'Sounalet', 'Alexandre', 'miamo.fr', '2002-02-01', 0);
+
+    // Listage des utilisateurs
+    listColumns(`${name}_user`, `${name}_cfuser`);
+
+}
+main();
+
+/**
+ * createKeyspace() ajoute à la base de données un keyspace à partir d'un nom donné
+ * @param {String} keyspace
+ */
+function createKeyspace(keyspace) {
+    client.execute(`CREATE KEYSPACE IF NOT EXISTS ${keyspace}
+    WITH replication = {
+        'class' : 'SimpleStrategy',
+        'replication_factor' : 3
+    }`);
+    console.log(`⭐ Keyspace ${keyspace} ajouté à la base`);
+}
+
+
+/**
+ * listColumns() liste toutes les colonnes de la table indiquée en paramètres
+ */
+function listColumns(keyspace, table) {
+    const query = `SELECT * FROM ${keyspace}.${table}`;
+    client.execute(query, []).then(result => console.table(result.rows));
+}
+
+/**
+ * createTable() crée une nouvelle table dans la base à partir d'un keyspace et d'un nom de table
+ * @param {String} keyspace Nom du keyspace existant
+ * @param {String} tablename Nom de la table à ajouter
+ */
+async function createTable(keyspace, tableName) {
+    const query = `CREATE TABLE IF NOT EXISTS ${keyspace}.${tableName}(
+		id UUID PRIMARY KEY,
+		lastname text,
+		name text,
+		email text,
+		dateNaissance date,
+		supprime boolean
+	);`;
+    await client.execute(query);
+    console.log(`⭐ Table ${tableName} ajoutée à la base`);
+}
+
+async function truncateTable(keyspace, tableName) {
+    const query = `TRUNCATE ${keyspace}.${tableName}`;
+    await client.execute(query);
+    console.log(`⭐ Table ${tableName} vidée`);
+}
+
+async function createUser(keyspace, columnFamily, id, lastname, name, email, dateNaissance, supprime) {
+    const query = `insert into ${keyspace}.${columnFamily} (id, lastname, name, email, dateNaissance, supprime) values (?, ?, ?, ?, ?, ?)`;
+    await client.execute(query, [id, lastname, name, email, dateNaissance, supprime]);
+    console.log(`⭐ Utilisateur ${name} ajouté à la base ${columnFamily}`);
+}
